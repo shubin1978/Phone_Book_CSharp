6beca3189dcd3bd3ee3913bd2b307c4437b24a9d
yury <shubin1978@omnicommdv.ru>
2024-03-05 10:18:11 +0800

    Создание контекста данных

---
+++ PhoneBook.DbLib/PhoneBookContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBook.Model;
+
+namespace PhoneBook.DbLib;
+
+public class PhoneBookContext :DbContext
+{
+    private readonly string _connectionString;
+    
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Phone> Phones { get; set; }
+    
+    protected PhoneBookContext(string connectionString)
+    {
+        _connectionString = connectionString;
+
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=phoneBookDB;Username=postgres;Password=1234580");
+        optionsBuilder.UseNpgsql(_connectionString);
+    }
+    
+}
