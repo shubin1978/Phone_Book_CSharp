cd03fb94515c6dff5528c5b021934fa5c3e1ec0d
yury <shubin1978@omnicommdv.ru>
2024-03-05 09:46:08 +0800

    Добавление модели данных

---
+++ PhoneBook.Model/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhoneBook.Model;
+
+//public record Person(int Id, string LastName, string? FirstName, List<Phone> Phones);
+[Table("table_persons")]
+public record Person
+{
+    [Column("id")]
+    [Required] // НЕ может быть NULL
+    public int Id { get; set; }
+    
+    [Column("last_name")]
+    [Required]
+    public string LastName { get; set; }
+    
+    [Column("first_name")]
+    public string? FirstName { get; set; }
+    
+    private IEnumerable<Phone> Phones { get; set; }
+}
---
+++ PhoneBook.Model/Phone.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace PhoneBook.Model;
+
+public enum PhoneType
+{
+    Unknown, Mobile, Home
+}
+[Table("table_phones")]
+[Index ("Number", IsUnique = true, Name = "phone_number_index")]
+public record Phone
+{
+    [Column("id")]
+    [Required]
+    public int Id { get; set; }
+    
+    [Column("phone_type")]
+    [Required]
+    public PhoneType Type { get; set; }
+    
+    [Column("number")]
+    [Required]
+    public string Number { get; set; }
+    
+    [Required]
+    [Column("person_id")]
+    public int PersonId { get; set; }
+    [ForeignKey("PersonId")]
+    public Person Person { get; set; }
+}
